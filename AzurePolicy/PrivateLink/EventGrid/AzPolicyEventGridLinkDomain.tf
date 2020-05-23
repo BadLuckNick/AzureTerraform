d5229f73a3051d5fbcb2861734b6e13a27a774df
Nicky Willebrand <32385627+BadLuckNick@users.noreply.github.com>
2020-05-23 23:54:08 +0200

    Create AzPolicyEventGridLinkDomain.tf

---
+++ AzurePolicy/PrivateLink/EventGrid/AzPolicyEventGridLinkDomain.tf
@@ -0,0 +1,48 @@
+resource "azurerm_policy_definition" "eventgriddomainprivatelink" {
+  name         = "EventGrid-domain-PrivateLink"
+  policy_type  = "Custom"
+  mode         = "Indexed"
+  display_name = "Audit Event Grid Domains for Private Link"
+
+  metadata     = <<METADATA
+    {
+    "category": "PrivateLink"
+    }
+  METADATA
+
+  policy_rule = <<POLICY_RULE
+    {
+      "if": {
+        "field": "type",
+        "equals": "Microsoft.EventGrid/domains"
+      },
+      "then": {
+        "effect": "[parameters('effect')]",
+        "details": {
+          "type": "Microsoft.EventGrid/domains/privateEndpointConnections",
+          "existenceCondition": {
+            "field": "Microsoft.EventGrid/domains/privateEndpointConnections/privateLinkServiceConnectionState.status",
+            "equals": "Approved"
+          }
+        }
+      }
+    }
+POLICY_RULE
+
+  parameters = <<PARAMETERS
+    {
+		"effect":{
+			"type": "String",
+			"metadata":{
+				"displayName": "Effect",
+				"description": "Enable or disable the execution of the policy"
+			},
+        "allowedValues": [
+          "AuditIfNotExists",
+          "Disabled"
+        ],
+        "defaultValue": "AuditIfNotExists"
+      }
+		}
+PARAMETERS
+}
